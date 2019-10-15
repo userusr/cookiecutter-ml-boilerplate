80f05e18a8c15e1b5621a2d79f03d3b5ef701cfe
userusr <userusr@yandex.ru>
2019-10-15 14:30:04 +0300

    Add basic project stracture

---
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/project.py
@@ -0,0 +1,19 @@
+import logging
+import logging.config
+from pathlib import Path
+
+import yaml
+
+
+class Coordinator:
+    def __init__(self, log_file_name="log.txt"):
+        self.path_project_root = Path(__file__).resolve().parents[1]
+        self.path_log = self.path_project_root / "log"
+        self.path_config = self.path_project_root / "config"
+
+        with open(self.path_config / "logging_config.yml", "r") as f:
+            config = yaml.safe_load(f.read())
+            config["handlers"]["file"]["filename"] = self.path_log / log_file_name
+
+            logging.config.dictConfig(config)
+            self.logger = logging.getLogger("root")
