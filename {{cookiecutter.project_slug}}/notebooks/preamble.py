97245ee7b7537b3ad81fdf7036314136680a6ac3
userusr <userusr@yandex.ru>
2019-10-15 15:11:12 +0300

    Use project_slug as source dir in preamble.py

--- {{cookiecutter.project_slug}}/notebooks/preamble.py
+++ {{cookiecutter.project_slug}}/notebooks/preamble.py
@@ -1,4 +1,5 @@
 import sys
 from pathlib import Path
 
-sys.path.insert(0, str(Path(__file__).resolve().parents[1] / "src"))
+source_dir = "{{cookiecutter.project_slug}}"
+sys.path.insert(0, str(Path(__file__).resolve().parents[1] / source_dir))
