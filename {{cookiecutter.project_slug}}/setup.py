8c40565bf5e1313714f18977d1045be9cd8f6351
userusr <userusr@yandex.ru>
2019-10-16 15:55:51 +0300

    Add requirevents to setup.py

--- {{cookiecutter.project_slug}}/setup.py
+++ {{cookiecutter.project_slug}}/setup.py
@@ -1,9 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""The setup script."""
+
 from setuptools import find_packages, setup
 
+with open('requirements.in') as requirements_file:
+     requirements = requirements_file.read()
+
 setup(
-    name='{{cookiecutter.project_slug}}',
+    name="{{cookiecutter.project_slug}}",
     packages=find_packages(),
-    version='{{cookiecutter.version}}',
-    description='{{ cookiecutter.project_short_description }}',
-    author='{{ cookiecutter.full_name }}'
+    install_requires=requirements,
+    version="{{cookiecutter.version}}",
+    description="{{ cookiecutter.project_short_description }}",
+    author="{{ cookiecutter.full_name }}",
 )
