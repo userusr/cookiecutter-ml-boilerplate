b2e6a261baebd9e5fa1f017be0f5a3b367597c1c
userusr <userusr@yandex.ru>
2019-10-21 15:11:02 +0300

    Project creation test

--- tests/test_bake_project.py
+++ tests/test_bake_project.py
@@ -24,6 +24,23 @@ def test_project_tree(cookies):
     assert result.exception is None
     assert result.project.basename == "test_project"
 
+    top_level_files = [
+        "config",
+        "data",
+        "log",
+        "models",
+        "notebooks",
+        "submit",
+        "Makefile",
+        "setup.py",
+        "requirements.in",
+        "requirements.txt",
+    ]
+
+    found_toplevel_files = [f.basename for f in result.project.listdir()]
+    for file_name in top_level_files:
+        assert file_name in found_toplevel_files
+
 
 def test_run_flake8(cookies):
     result = cookies.bake(extra_context={"project_slug": "flake8_compat"})
